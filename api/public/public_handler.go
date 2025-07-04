5fa57d7c6f2851cd35b94cebbe06cc4825be42d4
reinanbr <perseu912@gmail.com>
2025-07-04 14:33:39 -0300

    upload public try renderHtml

--- api/public/public_handler.go
+++ api/public/public_handler.go
@@ -2,19 +2,16 @@ package public
 
 import (
 	"net/http"
-	"fmt"
+	"os"
 )
 
 // Handler para servir arquivos estáticos do diretório public
 func PublicHandle(w http.ResponseWriter, r *http.Request) {
-	// Verifica se o arquivo requisitado existe
-	if _, err := http.Dir("./public").Open(r.URL.Path[len("/api/doc/"):]); err != nil {
-		http.Error(w, fmt.Sprintf(`{"error": "Arquivo não encontrado: %v | path: %v"}`, err, r.URL.Path), http.StatusNotFound)
-		fmt.Println("Erro ao servir arquivo:", err)
+	data, err := os.ReadFile("./public/index.html")
+	if err != nil {
+		http.Error(w, "Arquivo não encontrado", http.StatusNotFound)
 		return
 	}
-
-	fs := http.FileServer(http.Dir("./public"))
-	http.StripPrefix("/api/doc/", fs).ServeHTTP(w, r)
-	fmt.Println("Request received for:", r.URL.Path)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write(data)
 }
