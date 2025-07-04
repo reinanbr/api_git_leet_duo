66fa15458baf969784db487e4fb09884f76688be
reinanbr <perseu912@gmail.com>
2025-07-04 13:57:23 -0300

    upload public try 7

--- api/public/public_handler.go
+++ api/public/public_handler.go
@@ -7,8 +7,14 @@ import (
 
 // Handler para servir arquivos estáticos do diretório public
 func PublicHandle(w http.ResponseWriter, r *http.Request) {
+	// Verifica se o arquivo requisitado existe
+	if _, err := http.Dir("./public").Open(r.URL.Path[len("/api/doc/"):]); err != nil {
+		http.Error(w, fmt.Sprintf(`{"error": "Arquivo não encontrado: %v | path: %v"}`, err, r.URL.Path), http.StatusNotFound)
+		fmt.Println("Erro ao servir arquivo:", err)
+		return
+	}
+
 	fs := http.FileServer(http.Dir("./public"))
-	// Remove "/public/" do início da URL para mapear corretamente o arquivo
 	http.StripPrefix("/api/doc/", fs).ServeHTTP(w, r)
 	fmt.Println("Request received for:", r.URL.Path)
 }
